a7f25c475c6d89cf15d951b12759ad8a211211c2
Even Bryhn Bøe <evenbbo@stud.ntnu.no>
2021-11-05 02:23:43 +0100

    Added dynamic port support

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-const PORT = 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
